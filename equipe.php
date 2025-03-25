556ab058fd3b1178d5ca94a423edb0c4be1b6d17
Safuan Mahroum <sfuncloverm@gmail.com>
2025-03-25 14:57:55 +0100

    Mise en place projet

---
+++ equipe.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet" />
+    <title>L'équipe | Calufa-beer.com</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+    </main>
+    
+</body>
+</html>
