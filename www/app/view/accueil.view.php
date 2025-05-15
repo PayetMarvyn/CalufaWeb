ab5d5bc04e9646a17e00049be354aff464c0705f
tracyvv <valleetracy3@gmail.com>
2025-05-15 16:23:48 +0200

    biere.view.php

--- www/app/view/accueil.view.php
+++ www/app/view/accueil.view.php
@@ -38,7 +38,7 @@ $bieres = $query->fetchAll();
     </section>
 
     <section class="pub">
-        <h1>Notre premiér lancement !</h1>
+        <h1>Notre premier lancement !</h1>
         <h2>Phrase d'accroche</h2>
         </p>la video est seulement un test</p>
         <iframe width="760" height="415" src="https://www.youtube.com/embed/wloQQptLYeE?si=s1p3EOGIQH1Plhnn"
