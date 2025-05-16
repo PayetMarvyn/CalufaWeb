c44d1d9dbb0172588e59cbbca816ff51e79cbd02
PayetMarvyn <marvynpayet@gmail.com>
2025-05-16 12:02:15 +0200

    lien entre page cata et biere (à corriger)

--- www/app/view/catalogue.view.php
+++ www/app/view/catalogue.view.php
@@ -8,12 +8,13 @@ $bieres = $query->fetchAll();
 <main>
     <section class="carrousel">
         <?php foreach ($bieres as $biere): ?>
-            <div class="carrousel-carte">
+            <div class="carrousel-carte"> <a href="index.php?route=biere&id=<?= $biere["id"]?>">
                 <a href="etudiant.php?id=<?= $student['id'] ?>"><img src="<?= '../../../public/images/' . $biere['image_url'] ?>" alt="Bière <?= $biere['id_biere'] ?>" ></a>
                 <h2> <?= $biere['nom'] ?> </h2>
                 <h3>Phrase d'accroche</h3>
                 <p><?= $biere['description'] ?></p>
                 <button> En savoir plus +</button>
+            </a>
             </div>
         <?php endforeach; ?>
     </section>
