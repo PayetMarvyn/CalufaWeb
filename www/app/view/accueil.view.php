3ad86f0c48f9b4661f2ddf3cc12f47d5995f694e
Safuan Mahroum <sfuncloverm@gmail.com>
2025-05-16 11:11:39 +0200

    accueil

--- www/app/view/accueil.view.php
+++ www/app/view/accueil.view.php
@@ -9,7 +9,7 @@ $bieres = $query->fetchAll();
     <section class="carrousel">
         <?php foreach ($bieres as $biere): ?>
             <div class="carrousel-carte">
-                <a href="bière.html"><img src="<?= '../../../public/images/' . $biere['image_url'] ?>" alt="Bière <?= $biere['id_biere'] ?>" ></a>
+                <a href="etudiant.php?id=<?= $student['id'] ?>"><img src="<?= '../../../public/images/' . $biere['image_url'] ?>" alt="Bière <?= $biere['id_biere'] ?>" ></a>
                 <h2> <?= $biere['nom'] ?> </h2>
                 <h3>Phrase d'accroche</h3>
                 <p><?= $biere['description'] ?></p>
