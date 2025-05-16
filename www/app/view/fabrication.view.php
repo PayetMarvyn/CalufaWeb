154958b8665b24be8874f406c7c4d9a0c1613991
Neemati05 <mchinda.neemati@gmail.com>
2025-05-16 09:58:49 +0200

    fabrication

--- www/app/view/fabrication.view.php
+++ www/app/view/fabrication.view.php
@@ -1,5 +1,14 @@
 <main>
-    <section>
-        Page de fabrication
+<h1>La fabrication de nos bières</h1>
+
+    <section id="Introduction">
+        <p>Chez CALUFA, chaque bière est le fruit d'un trvail artisanal et acharné afin de pouvoir
+            vous des bières qualitatives avec des produits de hautes qualités.
+            De la sélection des ingrédients à la mise en bouteille, nous suivons des méthodes
+            traditionnelles tout en y portant notre touche personnelle.
+        </p>
     </section>
+
+    <h2>Le maltage</h2>
+    
 </main>
