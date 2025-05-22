54a61dad6e37948d5cbf913187ab861c7a108db7
PayetMarvyn <marvynpayet@gmail.com>
2025-05-22 11:13:12 +0200

    catalogue changer

--- www/app/view/catalogue.view.php
+++ www/app/view/catalogue.view.php
@@ -2,10 +2,10 @@
     <section class="catalogue">
         <?php foreach ($bieres as $biere): ?>
             <div class="catalogue-carte">
-                <a href="index.php?route=biere&id=<?= $biere['id_biere'] ?>"><img src="<?= '../../../public/images/' . $biere['image_url'] ?>" alt="Bière <?= $biere['id_biere'] ?>"></a>
+                <a href="index.php?route=biere&id=<?= $biere['id_biere'] ?>"><img src="<?= '../../../public/images/' . $biere['image_url'] ?>" alt="Bière <?= $biere['id_biere'] ?>">
                 <h2> <?= $biere['nom'] ?> </h2>
                 <p><?= $biere['description'] ?></p>
-                <a href="index.php?route=biere&id=<?= $biere['id_biere'] ?>"><button> En savoir plus +</button></a>
+                </a>
             </div>
         <?php endforeach; ?>
     </section>
