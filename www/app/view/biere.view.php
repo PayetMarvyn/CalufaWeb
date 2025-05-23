280a2e520106f28a48b0ccb324202eb35ad81b14
tracyvv <valleetracy3@gmail.com>
2025-05-23 12:09:48 +0200

    css page biere unique 1

--- www/app/view/biere.view.php
+++ www/app/view/biere.view.php
@@ -3,20 +3,41 @@
         <figure class="img_biere">
             <img src="<?= '../../../public/images/' . $biere['image_url'] ?>" alt="Bière <?= $biere['id_biere'] ?>">
         </figure>
-        <div class="infos_biere">
-            <h2 class="nom_biere"><?= $biere['nom'] ?></h2>
-            <p class="desc_biere"><?= $biere['description'] ?></p>
-            <h3 class="prix_biere"><?= $biere['prix'] ?></h3>
-            <button id="btn_panier" type="button">Ajouter au panier</button>
+        <div class="texte">
+            <div class="infos_biere">
+                <h2 class="nom_biere"><?= $biere['nom'] ?></h2>
+                <h2 class="prix_biere"><?= $biere['prix'] ?></h2>
+                <p>75ml</p>
+                <h3>Description</h3>
+                <p class="desc_biere"><?= $biere['description'] ?></p>
+                <h3>Taux d'alcool</h3>
+                <p class="taux_alcool"><?= $biere['taux_alcool'] ?></p>
+                <h3>Ingrédients</h3>
+                <p class="ingredients"><?= $biere['ingredients'] ?></p>
+            </div>
+            <div class="ajout_panier">
+                <select name="quantite" class="quantite">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <button id="btn_panier" type="button">Ajouter au panier</button>
+            </div>
         </div>
     </section>
 
     <section class="avis">
-        <fieldset>
-            <legend>Noter notre produit</legend>
-            <input type="text" id="texte_avis" placeholder="Laisser un avis...">
-            <input type="submit" id="btn_avis">
-        </fieldset>
-        <input type="range" min="1" max="5" id="star_rating">
+        <h2>Noter notre produit</h2>
+        <ul class="form_avis">
+            <li>
+                <input type="text" id="texte_avis" placeholder="Laisser un avis...">
+            </li>
+            <li>
+                <input type="submit" id="btn_avis">
+            </li>
+        </ul>
+        <!--<input type="range" min="1" max="5" id="star_rating">-->
     </section>
 </main>
