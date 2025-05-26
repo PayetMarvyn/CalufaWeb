425aaab15ccf24128e86e5c9a97dc7d2efb58cad
tracyvv <valleetracy3@gmail.com>
2025-05-26 14:07:26 +0200

    page histoire 1

--- www/app/view/histoire.view.php
+++ www/app/view/histoire.view.php
@@ -1,6 +1,32 @@
 <main>
-    <section>
-        Page de l'histoire
+    <h1>L'histoire de Calufa</h1>
+    <h2>Qui est Jorge ?</h2>
+    <section class="presentation_jorge">
+        <p>Jorge est une grenouille coquì, originaire de Porto Rico. 
+            Il arbore un style typiquement portoricain avec sa chemise 
+            bleu et son chapeau pava qu’il a fabriqué lui-même avec des 
+            feuilles de palmier. Il adore voyager pour découvrir de 
+            nouvelles saveurs et c’est ainsi qu’il a élaboré la recette 
+            de chacune des bières Calufa.
+        </p>
+        <figure class="img_jorge">
+            <img src="../../public/images/logo_calufa.png">
+        </figure>
+    </section>
+    <h2>L'origine de la bière</h2>
+    <section class="presentation_biere">
+        <p>
+            Le goût des bières Calufa est inspiré des cocktails emblématiques 
+            de l'Amérique latine. La Pina Colada de Porto Rico, le Fresay Coco 
+            de République Dominicaine, l'Ipanema du Brésil, le Chiliguaro du 
+            Costa Rica, le Tepache du Mexique et le Mojito de Cuba. Nos bières 
+            sont une invitation au voyage, à l'évasion et à la découverte de saveurs. 
+            Elles sont parfaites pour se rafraichir lors d'un moment de détente 
+            ou de fête.
+        </p>
+        <figure>
+            <img src="">
+        </figure>
     </section>
 
     <!-- Fonction du header au moment du scroll (a ne pas éffacer) -->
